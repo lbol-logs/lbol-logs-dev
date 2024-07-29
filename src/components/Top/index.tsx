98bd2a6e4b20c259913463f521d8e4f0f73134c0
imat-cww <imat.cww@gmail.com>
2024-07-29 18:42:11 +0900

    Update index.tsx

--- src/components/Top/index.tsx
+++ src/components/Top/index.tsx
@@ -1,11 +1,9 @@
 import { useParams } from 'react-router-dom';
-import { useTranslation, Trans } from 'react-i18next';
 import About from './about';
 import LanguageSwitcher from 'components/Common/languageSwitcher';
 
 function Top() {
   const { ver } = useParams<{ ver: string }>();
-  const { t } = useTranslation();
   /*
    * TODO: バージョン検証
    * サポート外の例外処理
