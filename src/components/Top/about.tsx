12dd92f7f2bed0017b8048670b5f67dcf8b8ca3c
ed-ev <93561881+ed-ev@users.noreply.github.com>
2024-07-29 19:52:15 +0900

    about.tsx を更新

--- src/components/Top/about.tsx
+++ src/components/Top/about.tsx
@@ -1,7 +1,9 @@
+import i18next from 'i18next';
 import { useTranslation, Trans } from 'react-i18next';
 
 function About() {
   const { t } = useTranslation();
+  const modUrl = `https://github.com/ed-ev/lbol-runLogger/blob/master/README${i18next.language === 'en' ? '' : '-' + i18next.language}.md`;
   /*
    * TODO: バージョン検証
    * サポート外の例外処理
@@ -10,10 +12,14 @@ function About() {
 
   return (
     <>
+      {t('underConstruction', { ns: 'common' })}
       <Trans
         i18nKey="about"
         ns='common'
-        components={{ a: <a href="https://store.steampowered.com/app/1140150/"></a> }}
+        components={{
+          steam: <a href="https://store.steampowered.com/app/1140150/"></a>,
+          mod: <a href={modUrl}></a>
+        }}
       />
     </>
   );
