1515c6211ba815696493f8b7e5c3d60f7e1cbb18
imat-cww <imat.cww@gmail.com>
2024-07-29 13:04:11 +0900

    i18next, read-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import i18next from 'i18next';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <link rel="manifest" href={`${process.env.PUBLIC_URL}/locales/${i18next.language}/manifest.json`} />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/configs/i18next.ts
@@ -0,0 +1,17 @@
+import i18next from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
+
+i18next
+  .use(resourcesToBackend((language, namespace, callback) => {
+    import(`/public/locales/${language}/${namespace}.json`)
+      .then((resources) => {
+        callback(null, resources)
+      })
+      .catch((error) => {
+        callback(error, null)
+      })
+  }))
+  .init({
+    lng: 'ja',
+    ns: ['common']
+  });
