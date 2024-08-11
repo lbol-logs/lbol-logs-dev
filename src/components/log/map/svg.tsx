2d81dd40a2cbf5c3777e6f34e45c08bd2a352307
imat-cww <imat.cww@gmail.com>
2024-08-11 12:15:01 +0900

    getLine

--- src/components/log/map/svg.tsx
+++ src/components/log/map/svg.tsx
@@ -2,7 +2,7 @@ import { LogContext } from 'contexts/logContext';
 import { useContext, useEffect, useState } from 'react';
 import { checkForce } from 'utils/checkForce';
 import MapNodes from 'utils/MapNodes';
-import { ExhibitWithCounter, TActObj, TLevel, TNode, TNodeObj, TNodeY } from 'utils/types/runData';
+import { TActObj, TNodeObj, TNodeX, TNodeY } from 'utils/types/runData';
 
 function Svg({ ActObj }: { ActObj: TActObj }) {
   const { runData, act, level, ignoredPaths } = useContext(LogContext);
@@ -15,21 +15,46 @@ function Svg({ ActObj }: { ActObj: TActObj }) {
   const h = force ? nodesY.filter(Y => Y).length : 1;
 
   const currentActStations = runData.Stations.map(({ Node }) => Node).filter(({ Act: _act }) => _act === Act);
-  function getStationY(level: TLevel) {
+  function getStationY(level: TNodeX) {
     const station = currentActStations.find(({ Level }) => Level === level) as TNodeObj;
     const Y = station && station.Y;
     return Y;
   }
 
-  let lastLevel: TLevel;
+  function getNodes(level: TNodeX, y?: TNodeY) {
+    const nodes = Nodes.filter(({ X, Y }) => X === level && (!y || Y === y));
+    return nodes;
+  }
+
+  type TLineArgs = {
+    X: TNodeX,
+    Y: TNodeY,
+    X2: TNodeX,
+    Y2: TNodeY,
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    color: string,
+    width: number,
+    dash?: number
+  };
+
+  function getLine({ X, Y, X2, Y2, x1, y1, x2, y2, color, width, dash = 0 }: TLineArgs) {
+    return (
+      <line key={`Act${Act}_x${X}y${Y}_x${X2}y${Y2}`} x1={x1} y1={y1} x2={x2} y2={y2} stroke={color} strokeWidth={width} strokeDasharray={dash}></line>
+    );
+  }
+
+  let lastLevel: TNodeX;
   const lines = Nodes.map(node => {
     const { X, Y, Followers } = node;
+    const X2 = X + 1 as TNodeX;
     const currentStationY = getStationY(X);
-    const nextStationY = getStationY(X + 1 as TLevel);
+    const nextStationY = getStationY(X2);
     lastLevel = X;
 
     return Followers.map(Follower => {
-      const X2 = X + 1;
       const Y2 = Follower as TNodeY;
       const [x1, x2] = MapNodes.x1x2(X);
       const [y1, y2] = MapNodes.y1y2(Y, Y2, force);
@@ -50,16 +75,9 @@ function Svg({ ActObj }: { ActObj: TActObj }) {
       const width = widths[key];
       const color = colors[key];
 
-      return (
-        <line key={`Act${Act}_x${X}y${Y}_x${X2}y${Y2}`} x1={x1} y1={y1} x2={x2} y2={y2} stroke={color} strokeWidth={width}></line>
-      );
+      return getLine({ X, Y, X2, Y2, x1, y1, x2, y2, color, width });
     });
   });
-
-  function getNodes(level: TLevel, y?: TNodeY) {
-    const nodes = Nodes.filter(({ X, Y }) => X === level && (!y || Y === y));
-    return nodes;
-  }
 
   // TiangouYuyi
   useEffect(() => {
@@ -76,23 +94,21 @@ function Svg({ ActObj }: { ActObj: TActObj }) {
 
     const startLevel = add.Station.Act === Act ? add.Station.Level + 1 : 0;
     const endLevel = (remove && remove.Station.Act === Act) ? remove.Station.Level : lastLevel;
-    console.log({act, remove, startLevel, endLevel});
     for (let l = startLevel; l < endLevel; l++) {
-      console.log({l, level});
       if (l < level) continue;
       else if (l > level) break;
-      
-      const X = l as TLevel;
+
+      const X = l as TNodeX;
       const Y = getStationY(X);
+      const X2 = X + 1 as TNodeX;
       const currentNode = getNodes(X, Y)[0];
-      const nextNodes = getNodes(X + 1 as TLevel);
+      const nextNodes = getNodes(X2);
       if (nextNodes.length === 0) break;
 
       const { Followers } = currentNode;
-      console.log(currentNode, nextNodes, { Y });
       for (const { Y: y } of nextNodes) {
         if (Followers.includes(y)) continue;
-        const X2 = X + 1;
+
         const Y2 = y;
         const [x1, x2] = MapNodes.x1x2(X);
         const [y1, y2] = MapNodes.y1y2(Y, Y2, force);
@@ -100,16 +116,13 @@ function Svg({ ActObj }: { ActObj: TActObj }) {
         const key = flag as keyof typeof widths;
         const width = widths[key];
         const color = colors[key];
-        const line = (
-          <line key={`Act${Act}_x${X}y${Y}_x${X2}y${Y2}`} x1={x1} y1={y1} x2={x2} y2={y2} stroke={color} strokeWidth={width} strokeDasharray={dash}></line>
-        );
+        const line = getLine({ X, Y, X2, Y2, x1, y1, x2, y2, color, width, dash });
         linesActive.push(line);
       }
     }
 
     const uses = ignoredPaths.filter(({ Type }) => Type === 'Use');
     console.log({uses});
-
 
     const test = Object.entries({}).map(([Level, Counter]) => {
       return (
