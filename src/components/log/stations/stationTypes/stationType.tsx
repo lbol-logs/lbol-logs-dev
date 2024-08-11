cab8af06ccbb9f718dfe42cbf12f8421224d1de8
imat-cww <imat.cww@gmail.com>
2024-08-11 19:04:10 +0900

    EventStation

--- src/components/log/stations/stationTypes/stationType.tsx
+++ src/components/log/stations/stationTypes/stationType.tsx
@@ -1,12 +1,14 @@
 import { TStation } from 'utils/types/runData';
 import BattleStation from './battleStation';
 import TempStation from './tempStation';
+import EventStation from './eventStation';
 
 function StatationType({ station }: { station: TStation }) {
   const { Type } = station;
 
   const battleStations = ['Enemy', 'Elite', 'Boss'];
   if (battleStations.includes(Type)) return <BattleStation station={station} />;
+  else if (Type === 'Adventure') return <EventStation station={station} />;
   else return <TempStation station={station} />;
 }
 
