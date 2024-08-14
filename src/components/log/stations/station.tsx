a364897ea0daf15851a857bc6127a0c5b02f4652
imat-cww <imat.cww@gmail.com>
2024-08-15 00:17:21 +0900

    fix station icon

--- src/components/log/stations/station.tsx
+++ src/components/log/stations/station.tsx
@@ -1,23 +1,41 @@
 import { RefObject, useContext, useEffect } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { TStation, TStatus } from 'utils/types/runData';
+import { TActObj, TStation, TStatus } from 'utils/types/runData';
 import Statuses from './parts/statuses';
 import { LogContext } from 'contexts/logContext';
 import CurrentChange from './currentChange';
 import StationType from './stationTypes/stationType';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { getMapImage } from 'utils/functions/getImage';
+import { getBossImage, getMapImage } from 'utils/functions/getImage';
 import { iconSize } from 'configs/globals';
 
 function Station({ station, innerRef }: { station: TStation, innerRef?: RefObject<HTMLDivElement>}) {
   const { t } = useTranslation();
   const { runData, setIsStationsLoaded } = useContext(LogContext);
   const { Node, Status, Type } = station;
-  const { Level } = Node;
+  const { Act, Level } = Node;
 
-  const { Stations } = runData;
+  const { Acts, Stations } = runData;
   const i = Stations.indexOf(station);
   const lastStatus = i ? Stations[i - 1].Status : runData.Settings.Status;
+
+  let src;
+  let type: string = Type;
+  const isBoss = type === 'Boss';
+
+  if (isBoss) {
+    const { Boss } = Acts.find(({ Act: _act }) => _act === Act) as TActObj;
+    type = Boss as string;
+    src = getBossImage(type);
+  }
+  else {
+    if (type === 'Enemy') {
+      if (Level < 10) type += 'Strong';
+      type += 2 - Level % 2;
+    }
+    if (type === 'Trade') type += Act;
+    src = getMapImage(type);
+  }
 
   useEffect(() => {
     if (innerRef) {
@@ -38,7 +56,7 @@ function Station({ station, innerRef }: { station: TStation, innerRef?: RefObjec
             }}
           />
         </h3>
-        <LazyLoadImage className="p-station__icon" src={getMapImage(Type)} width={iconSize} height={iconSize} alt={t(`stations.${Type}`, { ns: 'log' })} />
+        <LazyLoadImage className="p-station__icon" src={src} width={iconSize} height={iconSize} alt={t(`stations.${Type}`, { ns: 'log' })} />
         <Statuses status={Status} lastStatus={lastStatus as TStatus} />
       </div>
       <div className="p-station__body">
