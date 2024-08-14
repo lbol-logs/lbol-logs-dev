05d1a4d9ab1c70c140270f14aa775f1b76c88803
imat-cww <imat.cww@gmail.com>
2024-08-14 16:00:04 +0900

    c-requests

--- src/components/common/parts/requestsWidget.tsx
+++ src/components/common/parts/requestsWidget.tsx
@@ -11,17 +11,9 @@ function RequestsWidget({ requests }: { requests: TRequests }) {
         const active = requests.includes(request);
 
         return (
-          <div className="c-request" key={request}>
-            <span className="c-request__circle">
-              <i className={`c-request__dot ${active ? 'c-request__dot--active' : ''}`}></i>
-            </span>
-            <span className="c-request-name">
-              <span className="c-request-name__inner">
-                {t(`requests.${request}`, { ns: 'common' })}
-                <i className="c-request-name__arrow"></i>
-              </span>
-            </span>
-          </div>
+          <span className="c-request" key={request}>
+              <span className={`c-request__dot ${active ? 'c-request__dot--active' : ''}`} data-name={t(`requests.${request}`, { ns: 'common' })}></span>
+          </span>
         )
       })}
     </div>
