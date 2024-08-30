dc7daef25ba165d124a2d3b9ba8b7bb8d41d9109
imat-cww <imat.cww@gmail.com>
2024-08-30 17:52:13 +0900

    add JunkoColorless, baseMana extend

---
+++ src/components/log/stations/events/JunkoColorless.tsx
@@ -0,0 +1,68 @@
+import { TCards, TDialogueConfigs, TExhibits, TStation } from 'utils/types/runData';
+import DialogueWidget from '../parts/dialogueWidget';
+import { useContext } from 'react';
+import { TObjAny } from 'utils/types/common';
+import { convertCards, getNext } from 'utils/functions/helpers';
+import { LogContext } from 'contexts/logContext';
+import RewardsWidget from '../parts/rewardsWidget';
+import { MoneyImage } from '../parts/stationWidgets';
+import EventHead from '../parts/eventHead';
+
+function JunkoColorless({ station }: { station: TStation }) {
+  const { configsData } = useContext(LogContext);
+
+  const { Data, Id } = station;
+
+  const { Choices, Exhibit } = Data;
+
+  const id = Id as string;
+  const eventConfigs = configsData.events[id];
+  const configs = configsData.dialogues[id];
+
+  const { money, exhibit, misfortune} = eventConfigs;
+  const { current, next: options } = configs;
+
+
+  const [next] = getNext(options);
+  const chosen = Choices[0];
+
+  const props: Array<TObjAny> = [];
+  const cards: Array<TCards> = [];
+  const exhibits: Array<TExhibits> = [];
+
+  exhibits[0] = [exhibit];
+  const values = { 0: money };
+  const components = { Money: <MoneyImage /> };
+  props[1] = { values, components };
+  cards[1] = convertCards([misfortune]);
+  if (Exhibit) {
+    exhibits[1] = [Exhibit];
+  }
+
+  const dialogueConfigs: TDialogueConfigs = {
+    current,
+    next,
+    chosen,
+    props,
+    cards,
+    exhibits
+  };
+
+  return (
+    <div className="p-station__body">
+      <div className="p-station__main">
+        <div className="p-event">
+          <EventHead id={id} />
+          <div className="p-event__body">
+            <div className="p-dialogues">
+              <DialogueWidget id={id} dialogueConfigs={dialogueConfigs} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <RewardsWidget station={station} />
+    </div>
+  );
+}
+
+export default JunkoColorless;
--- src/components/log/stations/stationTypes/eventStation.tsx
+++ src/components/log/stations/stationTypes/eventStation.tsx
@@ -1,6 +1,7 @@
 import { TStation } from 'utils/types/runData';
 import EventDefault from '../events/eventDefault';
 import DoremyPortal from '../events/DoremyPortal';
+import JunkoColorless from '../events/JunkoColorless';
 
 function EventStation({ station }: { station: TStation }) {
   const { Id } = station;
@@ -8,6 +9,8 @@ function EventStation({ station }: { station: TStation }) {
   switch (Id) {
     case 'DoremyPortal':
       return <DoremyPortal station={station} />;
+    case 'JunkoColorless':
+      return <JunkoColorless station={station} />;
     default:
       return <EventDefault station={station} />;
   }
