1accf9292c316a881ef7b02ccfd67d842b402a4e
imat-cww <imat.cww@gmail.com>
2024-09-03 13:16:53 +0900

    Update choicesWidget.tsx

--- src/components/log/stations/parts/choicesWidget.tsx
+++ src/components/log/stations/parts/choicesWidget.tsx
@@ -1,29 +1,12 @@
-import { LogContext } from 'contexts/logContext';
-import { useContext } from 'react';
-import { useTranslation } from 'react-i18next';
 import { TChoice } from 'utils/types/others';
 
 function ChoicesWidget({ id, choices }: { id: string, choices: Array<TChoice> }) {
-  const { t } = useTranslation();
-  const { configsData } = useContext(LogContext);
-  const dialogueConfigs = configsData.dialogues;
-  let current = dialogueConfigs[id];
 
   // TODO REMOVE
   return (
     <div className="p-choices">
       {choices.map((choice, i) => {
-        // TODO
-        let dialogue;
-        if (current) {
-          const { line, next } = current[choice];
-          current = next;
-          dialogue = t(`${id}.${line}`, { ns: 'dialogues' });
-        }
-        else {
-          // TODO: remove
-          dialogue = `Choice #${i + 1}: ${choice}`;
-        }
+        const dialogue = `Choice #${i + 1}: ${choice}`;
 
         return (
           <div className="p-choice" key={i}>
