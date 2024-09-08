883d3bf5d4467f372bcb078717468699b88b1301
imat-cww <imat.cww@gmail.com>
2024-09-08 14:52:54 +0900

    fix A component suspended while responding to synchronous input

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Top from 'components/top';
 import Log from 'components/log';
 import About from 'components/about';
 import Upload from 'components/upload';
+import useVersion from 'hooks/useVersion';
 
 function Layout() {
   const { t } = useTranslation();
@@ -77,6 +78,7 @@ function App() {
     }
   ];
   const router = createBrowserRouter(routes);
+  useVersion();
 
   return (
     <RouterProvider router={router} />
--- src/hooks/useVersion.tsx
+++ src/hooks/useVersion.tsx
@@ -1,29 +1,30 @@
 import { commonConfigs, versions } from 'configs/globals';
 import { CommonContext } from 'contexts/commonContext';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { getConfigs } from 'utils/functions/fetchData';
 import use from 'utils/functions/use';
 import { TConfigsData } from 'utils/types/common';
 import { Navigate } from 'react-router-dom';
 
-function useVersion(ver: string)  {
-  const { version, setVersion, setConfigsData } = useContext(CommonContext);
+function useVersion(ver?: string)  {
+  const { version, setVersion, configsData, setConfigsData } = useContext(CommonContext);
 
+  ver = ver || version;
   const isChanged = ver !== version;
   const isValidVersion = versions.includes(ver);
-  const currentConfigs: TConfigsData = {};
-  for (const config of commonConfigs) {
-    const configs = use(getConfigs(ver, config));
-    currentConfigs[config] = configs;
-  }
 
   if (isChanged && isValidVersion) {
     setVersion(ver);
   }
 
-  useEffect(() => {
+  if (isChanged || commonConfigs.some(config => !(config in configsData))) {
+    const currentConfigs: TConfigsData = {};
+    for (const config of commonConfigs) {
+      const configs = use(getConfigs(ver, config));
+      currentConfigs[config] = configs;
+    }
     setConfigsData(currentConfigs);
-  }, [ver]);
+  }
 
   let redirect = null;
   if (!isValidVersion) redirect = <Navigate to="/" replace />;
