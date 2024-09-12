6d7445fc26eecf619679cc24e87a1f6c449bdce7
imat-cww <imat.cww@gmail.com>
2024-09-12 18:33:09 +0900

    fix `\t`

--- src/components/log/stations/stationTypes/stationType.tsx
+++ src/components/log/stations/stationTypes/stationType.tsx
@@ -12,28 +12,28 @@ import TradeStation from './tradeStation';
 function StationType({ station }: { station: TStation }) {
   const { Type } = station;
 
-	switch (Type) {
-		case 'Enemy':
-		case 'EliteEnemy':
-		case 'Boss':
-			return <BattleStation station={station} />;
-		case 'Adventure':
-			return <EventStation station={station} />;
-		case 'Gap':
-			return <GapStation station={station} />;
-		case 'Entry':
-			return <EntryStation station={station} />;
-		case 'Select':
-			return <SelectStation station={station} />;
-		case 'Supply':
-			return <SupplyStation station={station} />;
-		case 'Shop':
-			return <ShopStation station={station} />;
-		case 'Trade':
-			return <TradeStation station={station} />;
-		default:
-			return <TempStation station={station} />;
-	}
+  switch (Type) {
+    case 'Enemy':
+    case 'EliteEnemy':
+    case 'Boss':
+      return <BattleStation station={station} />;
+    case 'Adventure':
+      return <EventStation station={station} />;
+    case 'Gap':
+      return <GapStation station={station} />;
+    case 'Entry':
+      return <EntryStation station={station} />;
+    case 'Select':
+      return <SelectStation station={station} />;
+    case 'Supply':
+      return <SupplyStation station={station} />;
+    case 'Shop':
+      return <ShopStation station={station} />;
+    case 'Trade':
+      return <TradeStation station={station} />;
+    default:
+      return <TempStation station={station} />;
+  }
 }
 
 export default StationType;
