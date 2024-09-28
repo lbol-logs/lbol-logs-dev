6436ae4fae48939319540a19ca8ed0bb0d4a4ed4
imat-cww <imat.cww@gmail.com>
2024-09-28 11:34:28 +0900

    Update upgradeSwitcher.tsx

--- src/components/log/modal/upgradeSwitcher.tsx
+++ src/components/log/modal/upgradeSwitcher.tsx
@@ -1,6 +1,9 @@
+import { useTranslation } from 'react-i18next';
 import { TDispatch } from 'utils/types/common';
 
 function UpgradeSwitcher({ upgraded, setUpgraded }: { upgraded: boolean, setUpgraded: TDispatch<boolean> }) {
+  const { t } = useTranslation();
+
   function onChange() {
     setUpgraded(!upgraded);
   }
@@ -9,7 +12,7 @@ function UpgradeSwitcher({ upgraded, setUpgraded }: { upgraded: boolean, setUpgr
     <div className="p-card__upgrade-switcher c-card__center">
       <label className="c-upgrade-switcher">
         <input className="c-upgrade-switcher__toggle" type="checkbox" defaultChecked={upgraded} onChange={onChange} />
-        <span className="c-upgrade-switcher__text">Upgrade</span>
+        <span className="c-upgrade-switcher__text">{t('Upgrade', { ns: 'log' })}</span>
       </label>
     </div>
   );
