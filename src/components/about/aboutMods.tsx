11e927ccfa767c2d812214b42ec35fc5a636268b
imat-cww <imat.cww@gmail.com>
2024-10-05 20:26:36 +0900

    minor

--- src/components/about/aboutMods.tsx
+++ src/components/about/aboutMods.tsx
@@ -14,8 +14,8 @@ function AboutMods() {
 
   return (
     <div className="p-about-lbol-logs__block">
-      <h2 className="p-about-lbol-logs__supported-character-mods">
-        {t('lbolLogs.supportedCharacterMods', { ns: 'site' })}
+      <h2 className="p-about-lbol-logs__mods">
+        <span className="p-mods__text">{t('lbolLogs.supportedCharacterMods', { ns: 'site' })}</span>
         <VersionWidget className="p-mods__version" version={version} setVersion={setVersion} versionSwitch={true} />
       </h2>
       <ModsWidget mods={mods} />
