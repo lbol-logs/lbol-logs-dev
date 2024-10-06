41edbc614fa0e8f193c143c86baaa76bad92e791
imat-cww <imat.cww@gmail.com>
2024-10-06 14:58:21 +0900

    fix mod enemy ns

--- src/components/log/stations/parts/enemyCards.tsx
+++ src/components/log/stations/parts/enemyCards.tsx
@@ -2,9 +2,10 @@ import LazyLoadImage2 from 'components/common/utils/lazyLoadImage2';
 import { iconSize } from 'configs/globals';
 import { useTranslation } from 'react-i18next';
 import { getUnitImage } from 'utils/functions/getImage';
+import { getNs } from 'utils/functions/helpers';
 
 function EnemyCards({ enemies }: { enemies: Array<string> }) {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const size = iconSize * 2;
 
@@ -24,10 +25,16 @@ function EnemyCards({ enemies }: { enemies: Array<string> }) {
             break;
         }
 
+        let ns = 'units';
+        if (!i18n.exists(name, { ns })) {
+          [ns] = getNs({ ns, isMod: true });
+        }
+        const text = t(name, { ns });
+
         return (
           <div className="p-enemy" key={i}>
             <LazyLoadImage2 className="p-enemy__img" callback={getUnitImage} name={name} width={size} height={size} alt="" />
-            <span className="p-enemy__text">{t(name, { ns: 'units' })}</span>
+            <span className="p-enemy__text">{text}</span>
           </div>
         );
       })}
