0bb8385f04b9eb3ef0f8a8f9d360ea7bb95ccc72
imat-cww <imat.cww@gmail.com>
2024-10-18 18:32:40 +0900

    adjust shop price

--- src/components/log/entityCards/cardCards.tsx
+++ src/components/log/entityCards/cardCards.tsx
@@ -3,30 +3,30 @@ import CardCard from './cardCard';
 import LazyLoadImage2 from 'components/common/utils/lazyLoadImage2';
 import { getStationImage } from 'utils/functions/getImage';
 import { iconSize } from 'configs/globals';
-import { Fragment } from 'react/jsx-runtime';
 
 function CardCards({ cards, added }: { cards: TCards | TCardsWithPrice, added?: Array<number> }) {
   return (
     <div className="c-entities c-cards">
       {cards.map((card, i) => {
         const isNotAdded = Boolean(added && !added.includes(i));
-        let price;
         const { Price, IsDiscounted } = card as TCardWithPrice;
-        if (Price !== undefined) {
-          price = (
-            <span className="c-price">
-              {Price}
-              {IsDiscounted && <LazyLoadImage2 callback={getStationImage} name="Discount" width={iconSize} height={iconSize / 2} alt="" />}
-            </span>
+
+        if (Price === undefined) {
+          return (
+            <CardCard card={card as TCard} isNotAdded={isNotAdded} key={i} />
           );
         }
-
-        return (
-          <Fragment key={i}>
-            <CardCard card={card as TCard} isNotAdded={isNotAdded} />
-            {price}
-          </Fragment>
-        );
+        else {
+          return (
+            <div className="c-entity--with-price" key={i}>
+              <CardCard card={card as TCard} isNotAdded={isNotAdded} />
+              <span className="c-price">
+                {Price}
+                {IsDiscounted && <LazyLoadImage2 callback={getStationImage} name="Discount" width={iconSize} height={iconSize / 2} alt="" />}
+              </span>
+            </div>
+          );
+        }
       })}
     </div>
   );
--- src/components/log/entityCards/exhibitCards.tsx
+++ src/components/log/entityCards/exhibitCards.tsx
@@ -2,31 +2,29 @@ import { TExhibitChanges, TExhibitObjs, TExhibits } from 'utils/types/runData';
 import ExhibitCard from './exhibitCard';
 import { TObjNumber } from 'utils/types/common';
 import { getExhibitId } from 'utils/functions/helpers';
-import { Fragment } from 'react/jsx-runtime';
 
 function ExhibitCards({ exhibits, added, prices }: { exhibits: TExhibits | TExhibitObjs | TExhibitChanges, added?: Array<number>, prices?: TObjNumber }) {
   return (
     <div className="c-entities c-exhibits">
       {exhibits.map((exhibit, i) => {
         const isNotAdded = Boolean(added && !added.includes(i));
-        let price;
-        if (prices) {
-          const Id = getExhibitId(exhibit);
-          if (Id in prices) {
-            price = (
-              <span className="c-price">
-                {prices[Id]}
-              </span>
-            );
-          }
-        }
-
-        return (
-          <Fragment key={i}>
+        if (prices === undefined) {
+          return (
             <ExhibitCard exhibit={exhibit} isNotAdded={isNotAdded} />
-            {price}
-          </Fragment>
-        );
+          );
+        }
+        else {
+          const Id = getExhibitId(exhibit);
+          const price = prices[Id];
+          return (
+            <div className="c-entity--with-price" key={i}>
+              <ExhibitCard exhibit={exhibit} isNotAdded={isNotAdded} />
+              <span className="c-price">
+                {price}
+              </span>
+            </div>
+          );
+        }
       })}
     </div>
   );
