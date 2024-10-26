19c7b30038f0badbbc4ec1b2e21ba459d282417f
imat-cww <imat.cww@gmail.com>
2024-10-26 12:13:08 +0900

    fix X post

--- src/components/log/modal/shareModal.tsx
+++ src/components/log/modal/shareModal.tsx
@@ -1,4 +1,3 @@
-import ExternalLink from 'components/common/parts/externalLink';
 import LazyLoadImage2 from 'components/common/utils/lazyLoadImage2';
 import { ogpBaseUrl } from 'configs/globals';
 import { useLocation } from 'react-router-dom';
@@ -20,6 +19,11 @@ function ShareModal() {
     navigator.share({ url });
   }
 
+  function post() {
+    const tweet = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`;
+    window.open(tweet, 'blank');
+  }
+
   return (
     <div className="p-modal__share">
       <div className="p-modal__body">
@@ -31,16 +35,14 @@ function ShareModal() {
           <span className="c-share__button" onClick={copy}>
             <LazyLoadImage2 callback={getCommonImage} name="Copy" alt="Copy" />
           </span>
-          {/* <span className="c-share__button" onClick={copy}></span> */}
           <span className="c-share__button" onClick={share}>
             <LazyLoadImage2 callback={getCommonImage} name="Share" alt="Share" />
           </span>
-          <ExternalLink className="c-share__button" href={`https://twitter.com/intent/tweet?url=${url}`}>
+          <span className="c-share__button" onClick={post}>
             <svg className="c-share__svg" width="1200" height="1227" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="white"/>
+              <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
             </svg>
-
-          </ExternalLink>
+          </span>
         </div>
       </div>
     </div>
