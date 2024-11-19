cdb6b76cf86c42e802fcd5ebe94ba5ac4042b17e
imat-cww <imat.cww@gmail.com>
2024-11-19 11:34:34 +0900

    copy Context Provider

---
+++ src/contexts/cardPoolContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, ReactNode, useState } from 'react';
+import { TDispatch } from 'utils/types/common';
+import { TFilter, TRunList } from 'utils/types/others';
+
+const defaultFilter: TFilter = {};
+const defaultShowFilter: boolean = false;
+const defaultList: TRunList = [];
+const defaultFilteredList: TRunList = [];
+
+type TCardPoolContext = {
+  filter: TFilter
+  setFilter: TDispatch<TFilter>,
+  showFilter: boolean,
+  setShowFilter: TDispatch<boolean>,
+  list: TRunList,
+  setList: TDispatch<TRunList>,
+  filteredList: TRunList,
+  setFilteredList: TDispatch<TRunList>
+};
+
+export const CardPoolContext = createContext<TCardPoolContext>({
+  filter: defaultFilter,
+  setFilter: () => {},
+  showFilter: defaultShowFilter,
+  setShowFilter: () => {},
+  list: defaultList,
+  setList: () => {},
+  filteredList: defaultFilteredList,
+  setFilteredList: () => {}
+});
+
+function CardPoolProvider({ children }: { children: ReactNode }) {
+  const [filter, setFilter] = useState(defaultFilter);
+  const [showFilter, setShowFilter] = useState(defaultShowFilter);
+  const [list, setList] = useState(defaultList);
+  const [filteredList, setFilteredList] = useState(defaultFilteredList);
+
+  const value = {
+    filter,
+    setFilter,
+    showFilter,
+    setShowFilter,
+    list,
+    setList,
+    filteredList,
+    setFilteredList
+  };
+
+  return (
+    <CardPoolContext.Provider value={value}>
+      {children}
+    </CardPoolContext.Provider>
+  );
+}
+
+export default CardPoolProvider;
