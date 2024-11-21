5fa37725201ef7dd16b80099bc1f8933f4f2ba84
imat-cww <imat.cww@gmail.com>
2024-11-21 15:19:38 +0900

    minor

--- src/components/pool/filters/customFilters/colorFilter.tsx
+++ src/components/pool/filters/customFilters/colorFilter.tsx
@@ -9,13 +9,15 @@ function ColorFilter({ onChange }: { onChange: ChangeEventHandler }) {
   const { co } = filter;
   const { t } = useTranslation();
 
+  const colors = 'WUBRGC'.split('');
+
   return (
     <>
       <div className="p-filter__row">
         <div className="p-filter__label">{t('color', { ns: 'site' })}</div>
         <div className="p-filter__values">
-          {'WUBRGC'.split('').map(color => {
-            const isChecked = co ? co.includes(color): false;
+          {colors.map(color => {
+            const isChecked = co ? co.includes(color) : false;
 
             return (
               <label className={`p-filter__toggle ${toggleIsChecked(isChecked)} u-button`} key={color}>
