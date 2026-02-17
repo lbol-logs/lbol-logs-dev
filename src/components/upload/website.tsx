a2374be2bd5031655eddf9fd35d8073aa4cc2f1f
ed-ev <93561881+ed-ev@users.noreply.github.com>
2026-02-17 18:13:17 +0900

    add `Website` to `uploader`

---
+++ src/components/upload/website.tsx
@@ -0,0 +1,31 @@
+import { websiteTitleMaxLength, websiteUrlMaxLength } from 'configs/globals';
+import { useTranslation } from 'react-i18next';
+
+function Website() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="p-upload__website">
+      <div className="p-upload-website__line">
+        <div className="p-upload-website__title">
+          <h3 className="p-upload-website__head">Website</h3>
+          <span className="p-upload-website__optional">({t('optional', { ns: 'site' })})</span>
+        </div>
+      </div>
+      <div className="p-upload-website__line">
+        <label>
+          <span className="p-upload-website__label">Title</span>
+          <input className="p-upload-website__textarea js-title" maxLength={websiteTitleMaxLength} />
+        </label>
+      </div>
+      <div className="p-upload-website__line">
+        <label>
+          <span className="p-upload-website__label">URL</span>
+          <input className="p-upload-website__textarea js-url" maxLength={websiteUrlMaxLength} />
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default Website;
